87f506308529e041cec35f4f19571cea2701527f
coneneco <43073341+coneneco@users.noreply.github.com>
2018-09-09 22:24:41 +0200

    First commit!

---
+++ src/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Test {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "Nemanja", "12345");	
+		
+		String query = "insert into primer (kolona) values (321)";
+		Statement st = conn.createStatement();
+		
+		st.executeQuery(query);
+	}
+
+}
